3620518e230c28f43ce4a1f31c28b2dd26c8fffa
ORANCEANU STEFAN - CRISTIAN <oranceanustefan23@stud.ase.ro>
2025-05-13 23:42:37 +0300

    Add files via upload

---
+++ ListaDublaL5.c
@@ -0,0 +1,111 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//functioneaza cu fisierul masini.txt
+
+typedef struct StructuraMasina {
+	int id;
+	int nrUsi;
+	float pret;
+	char* model;
+	char* numeSofer;
+	unsigned char serie;
+} Masina;
+
+typedef struct Nod {
+	Masina info;
+	struct Nod* prev;
+	struct Nod* next;
+} Nod;
+
+typedef struct ListaDubla {
+	Nod* prim;
+	Nod* ultim;
+} LD;
+
+Masina citireMasinaDinFisier(FILE* file) {
+	char buffer[100];
+	char sep[3] = ",\n";
+	fgets(buffer, 100, file);
+	char* aux;
+	Masina m1;
+	aux = strtok(buffer, sep);
+	m1.id = atoi(aux);
+	m1.nrUsi = atoi(strtok(NULL, sep));
+	m1.pret = atof(strtok(NULL, sep));
+	aux = strtok(NULL, sep);
+	m1.model = malloc(strlen(aux) + 1);
+	strcpy(m1.model, aux);
+
+	aux = strtok(NULL, sep);
+	m1.numeSofer = malloc(strlen(aux) + 1);
+	strcpy(m1.numeSofer, aux);
+
+	m1.serie = *strtok(NULL, sep);
+	return m1;
+}
+
+void afisareMasina(Masina masina) {
+	printf("Id: %d\n", masina.id);
+	printf("Nr. usi: %d\n", masina.nrUsi);
+	printf("Pret: %.2f\n", masina.pret);
+	printf("Model: %s\n", masina.model);
+	printf("Nume sofer: %s\n", masina.numeSofer);
+	printf("Serie: %c\n\n", masina.serie);
+}
+
+void afisareListaMasiniDeLaInceput(LD lista) {
+	Nod* p = lista.prim;
+	while (p) {
+		afisareMasina(p->info);
+		p = p->next;
+	}
+}
+
+void afisareListaMasiniDeLaSfarsit(LD lista) {
+	Nod* p = lista.ultim;
+	while (p) {
+		afisareMasina(p->info);
+		p = p->prev;
+	}
+}
+
+void adaugaMasinaInLista(LD* lista, Masina masinaNoua) {
+	Nod* nou = (Nod*)malloc(sizeof(Nod));
+	nou->info = masinaNoua;
+	nou->prev = NULL;
+	nou->next = NULL;
+	if (lista->prim != NULL) {
+		nou->prev = lista->ultim;
+		lista->ultim->next = nou;
+		lista->ultim = nou;
+	}
+	else {
+		lista->prim = nou;
+		lista->ultim = nou;
+	}
+}
+
+// Optional: Implementare pentru alte funcții cerute în enunț (le pot completa dacă vrei)
+
+int main() {
+	LD lista;
+	lista.prim = NULL;
+	lista.ultim = NULL;
+
+	FILE* f = fopen("masini.txt", "r");
+	if (f) {
+		while (!feof(f)) {
+			Masina m = citireMasinaDinFisier(f);
+			adaugaMasinaInLista(&lista, m);
+		}
+		fclose(f);
+	}
+
+	printf("Masinile din lista (de la inceput):\n");
+	afisareListaMasiniDeLaInceput(lista);
+
+	return 0;
+}
